bcf85cc0112be14bdebdb9349b878114c7a2a147
1heo2fine <156608112+1heo2fine@users.noreply.github.com>
2026-02-26 07:56:00 -0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,6 @@
+local args = { "collect" }
+local remote = game:GetService("ReplicatedStorage"):WaitForChild("Valentine2026"):WaitForChild("RemoteEvent")
 
+game:GetService("RunService").Heartbeat:Connect(function()
+    remote:FireServer(unpack(args))
+end)
